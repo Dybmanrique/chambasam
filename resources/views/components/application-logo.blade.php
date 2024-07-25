d1b36386a7c6cb08901d9bcc1554488ced4edf89
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-24 21:58:11 -0500

    Agregando el logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,5 @@
 <h1 class="text-4xl dark:text-white">
-    Chamba<span class="font-extrabold">SAM</span>
+    <div class="flex">
+        <img src="{{asset('img/logo_unasam.png')}}" alt="Logo unasam" class="w-10 mx-2">Chamba<span class="font-extrabold">SAM</span>
+    </div>
 </h1>
